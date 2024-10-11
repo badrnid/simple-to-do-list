eeb51de3f0af207b7ae5cf7bda6b85d10cfd24a4
Badr Nidalha <badrnidalhainfo@gmail.com>
2024-10-11 21:08:47 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,103 @@
+
+let input = document.querySelector('.input');
+let add = document.querySelector('.add');
+let tasks = document.querySelector('.tasks');
+
+let tasksArray = [];
+
+
+// ##### LOAD TASKS #####
+if (localStorage.getItem('tasks')) {
+    tasksArray = JSON.parse(localStorage.getItem('tasks'));
+    addTaskToForm(tasksArray);
+}
+// ######################
+
+add.onclick = function () {
+    if (input.value === "") {
+        alert('Please enter a valid text');
+    }
+    if (input.value !== "") {
+        addTaskToArray(input.value);
+        input.value = "";
+    }
+};
+
+function addTaskToArray(task) {
+    const taskData = {
+     id: Date.now(),
+     title: task,
+     completed: false,
+    };
+
+    tasksArray.push(taskData);
+
+    
+
+    // ######### STORE TASKS ###########
+
+    localStorage.setItem('tasks', JSON.stringify(tasksArray));
+
+    // #################################
+    addTaskToForm(tasksArray);
+}
+
+function addTaskToForm(tasksArray) {
+
+    tasks.innerHTML = ""; // Empty tasks div
+
+    tasksArray.forEach((taskData) => {
+        let div = document.createElement('div');
+        div.className = "task";
+
+        if (taskData.completed) {
+            div.classList.add('done');
+        }
+
+        div.setAttribute("data-id", taskData.id);
+        div.appendChild(document.createTextNode(taskData.title));
+        
+        // div.onclick = function () {
+        //     div.classList.toggle('done'); 
+        //     taskCompleted(taskData.id);
+             
+        // }
+
+        tasks.appendChild(div);
+
+        let span = document.createElement('span');
+        span.className = "del";
+        span.appendChild(document.createTextNode("Delete"));
+        div.appendChild(span);
+
+        
+        span.onclick = function () {
+            deleteTask(taskData.id);
+        };
+
+        tasks.appendChild(div);
+               
+        
+    });
+}
+function deleteTask(id) {
+
+        tasksArray = tasksArray.filter((task) => task.id !== id);
+
+        localStorage.setItem('tasks', JSON.stringify(tasksArray));
+        addTaskToForm(tasksArray);
+
+    }
+    // function taskComplete(id) {
+    //     tasksArray = tasksArray.map((task) => {
+    //         if (task.id === id) {
+    //             task.completed = !task.completed;
+    //         }
+             
+    //       localStorage.setItem('tasks', JSON.stringify(tasksArray));
+    //       addTaskToForm(tasksArray);
+    //         return task;
+            
+    //     });
+
+    // }
